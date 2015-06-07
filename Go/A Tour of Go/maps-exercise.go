ee3263e035f9b8c08d60974f5dfa8464cf6e9871
Cami Diez <diezcami@gmail.com>
2015-06-08 02:05:55 +0800

    Added Go Files

---
+++ Go/A Tour of Go/maps-exercise.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+    "strings"
+)
+
+func WordCount(s string) map[string]int {
+    words := strings.Fields (s)
+    counter := make (map[string] int)
+    
+    for _, word := range words {
+        if _, ok := counter[word] ; ok {
+            counter[word]++
+        } else {
+            counter[word] = 1
+        }
+    }
+    
+    return counter
+}
+
+func main() {
+    wc.Test(WordCount)
+}
+
